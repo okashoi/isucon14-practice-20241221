b8dc9efbb86c4e093b31f9508e13ab011baf06f0
OKADA Shohei <okashoicircus0@gmail.com>
2025-01-04 16:54:14 +0900

    いったん講評のコードをそのまま載せてみる

--- go/internal_handlers.go
+++ go/internal_handlers.go
@@ -1,101 +1,84 @@
 package main
 
 import (
-	"database/sql"
-	"errors"
 	"net/http"
+	"time"
 )
 
 // このAPIをインスタンス内から一定間隔で叩かせることで、椅子とライドをマッチングさせる
 func internalGetMatching(w http.ResponseWriter, r *http.Request) {
-	ctx := r.Context()
-
+	// 決まっていないライドと空いている椅子を全て取得
 	rides := []Ride{}
-	if err := db.SelectContext(ctx, &rides, `SELECT * FROM rides WHERE chair_id IS NULL`); err != nil {
-		if errors.Is(err, sql.ErrNoRows) {
-			w.WriteHeader(http.StatusNoContent)
-			return
-		}
-		writeError(w, http.StatusInternalServerError, err)
-		return
-	}
-
-	type CandidateChair struct {
-		ID        string `db:"id"`
-		Speed     int    `db:"speed"`
-		Latitude  int    `db:"latitude"`
-		Longitude int    `db:"longitude"`
-	}
-	candidates := []CandidateChair{}
-
-	q := `
-SELECT
-	c.id AS id,
-	cm.speed AS speed,
-	lcl.latitude AS latitude,
-	lcl.longitude AS longitude
-FROM
-	chairs c
-	INNER JOIN chair_models cm
-		ON c.model = cm.name
-	LEFT JOIN latest_chair_locations lcl
-		ON c.id = lcl.chair_id
-	LEFT JOIN (
-		SELECT
-			r.chair_id,
-			lrs.status
-		FROM
-			rides r
-		LEFT JOIN latest_ride_statuses lrs
-				ON r.id = lrs.ride_id AND lrs.chair_sent_at IS NOT NULL
-		WHERE
-			r.updated_at = (
-				SELECT MAX(sub_r.updated_at)
-				FROM rides sub_r
-				WHERE sub_r.chair_id = r.chair_id
-			)
-	) lr
-		ON c.id = lr.chair_id
-WHERE
-	c.is_active = TRUE AND
-	(lr.status = 'COMPLETED' OR lr.status IS NULL)
-`
-
-	if err := db.SelectContext(ctx, &candidates, q); err != nil {
-		if errors.Is(err, sql.ErrNoRows) {
-			w.WriteHeader(http.StatusNoContent)
-			return
-		}
-		writeError(w, http.StatusInternalServerError, err)
-		return
-	}
-	if len(candidates) == 0 {
+	if err := db.Select(&rides, `SELECT * FROM rides WHERE chair_id IS NULL ORDER BY created_at ASC`); err != nil {
 		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	if len(rides) <= 0 {
+		return
+	}
+
+	type ChairWithLatLon struct {
+		ID          string    `db:"id"`
+		OwnerID     string    `db:"owner_id"`
+		Name        string    `db:"name"`
+		Model       string    `db:"model"`
+		IsActive    bool      `db:"is_active"`
+		AccessToken string    `db:"access_token"`
+		CreatedAt   time.Time `db:"created_at"`
+		UpdatedAt   time.Time `db:"updated_at"`
+
+		Latitude  int `db:"latitude"`
+		Longitude int `db:"longitude"`
+	}
+	chairs := []ChairWithLatLon{}
+	if err := db.Select(&chairs, `
+ WITH chair_latest_location AS (
+ 	SELECT *
+ 	FROM (
+ 		SELECT chair_locations.*, ROW_NUMBER() OVER (PARTITION BY chair_id ORDER BY created_at DESC) AS rn
+ 		FROM chair_locations
+ 	) c
+ 	WHERE c.rn = 1
+ ),
+ chair_latest_status AS (
+ 	SELECT *
+ 	FROM (
+ 		SELECT rides.*, ride_statuses.status AS ride_status, ROW_NUMBER() OVER (PARTITION BY chair_id ORDER BY ride_statuses.created_at DESC) AS rn
+ 		FROM rides INNER JOIN ride_statuses ON rides.id = ride_statuses.ride_id AND ride_statuses.chair_sent_at IS NOT NULL -- この条件は椅子の通知エンドポイントの実装で、未送信の状態がある2つ以上の異なるライドが割り当てられていても正しく順番に送るように修正していれば不要
+ 	) r
+ 	WHERE r.rn = 1
+ )
+ SELECT
+ 	chairs.*, chair_latest_location.latitude, chair_latest_location.longitude
+ FROM chairs
+ LEFT JOIN chair_latest_status ON chairs.id = chair_latest_status.chair_id
+ LEFT JOIN chair_latest_location ON chairs.id = chair_latest_location.chair_id
+ WHERE
+ 	(chair_latest_status.ride_status = 'COMPLETED' OR chair_latest_status.ride_status IS NULL) AND chairs.is_active`); err != nil {
+		writeError(w, http.StatusInternalServerError, err)
 		return
 	}
 
 	for _, ride := range rides {
+		minDistance := 400
+		var minChair *ChairWithLatLon
 		var minChairIdx int
-		var minChair *CandidateChair
-		var minEstimatedTime float32
-		// distanceFromPickupToDestination := abs(ride.PickupLatitude-ride.DestinationLatitude) + abs(ride.PickupLongitude-ride.DestinationLongitude)
-		for i, chair := range candidates {
-			// 配車位置までの移動時間を算出
-			distanceToPickup := abs(chair.Latitude-ride.PickupLatitude) + abs(chair.Longitude-ride.PickupLongitude)
-			estimatedTime := float32(distanceToPickup) / float32(chair.Speed)
-			if minChair == nil || estimatedTime < minEstimatedTime {
-				minChairIdx = i
+		for idx, chair := range chairs {
+			distance := calculateDistance(chair.Latitude, chair.Longitude, ride.PickupLatitude, ride.PickupLongitude)
+			if distance < minDistance {
+				minDistance = distance
 				minChair = &chair
-				minEstimatedTime = estimatedTime
+				minChairIdx = idx
 			}
 		}
 		if minChair != nil {
-			if _, err := db.ExecContext(ctx, "UPDATE rides SET chair_id = ? WHERE id = ?", minChair.ID, ride.ID); err != nil {
+			// 複数のmatcherが動く場合には、複数の椅子が同じライドに割り当てられないようトランザクションなどで排他制御を行う必要がある
+			if _, err := db.Exec("UPDATE rides SET chair_id = ? WHERE id = ?", minChair.ID, ride.ID); err != nil {
 				writeError(w, http.StatusInternalServerError, err)
 				return
 			}
 
-			candidates = append(candidates[:minChairIdx], candidates[minChairIdx+1:]...)
+			chairs = append(chairs[:minChairIdx], chairs[minChairIdx+1:]...)
 		}
 	}
 
